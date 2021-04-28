6cb8adb9ba35708ce8757d7f3a35e5b75f7e9bd9
lesteenman <lesteenman@gmail.com>
2021-04-28 11:42:24 +0200

    Support for the clock channel

---
+++ discord_bot_the_eternal_gem/clock_channel/name_creator.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from math import floor
+
+
+def name(time: datetime):
+    hour = time.hour
+    minutes = int(floor(time.minute / 10) * 10)
+
+    if minutes == 0:
+        minutes = "00"
+
+    emoji = emoji_lookup[hour % 12]
+
+    return f"{emoji} {hour}:{minutes} UTC"
+
+
+emoji_lookup = {
+    0: '🕛',
+    1: '🕐',
+    2: '🕑',
+    3: '🕒',
+    4: '🕓',
+    5: '🕔',
+    6: '🕕',
+    7: '🕖',
+    8: '🕗',
+    9: '🕘',
+    10: '🕙',
+    11: '🕚',
+}
--- discord_bot_the_eternal_gem/run.py
+++ discord_bot_the_eternal_gem/run.py
@@ -1,26 +1,33 @@
 from argparse import ArgumentParser
 
-from discord_bot_the_eternal_gem.clock_channel.client import ClockChannelClient
-from discord_bot_the_eternal_gem.config import get_discord_token
-
 from loguru import logger as log
+
+from discord_bot_the_eternal_gem.clock_channel.client import ClockChannelClient
 
 MODE_CLOCK_CHANNEL = 'clock-channel'
 
 
-def start_clock_channel(arguments):
-    log.info("starting clock channel")
-    log.info(f"arguments={arguments}")
-    client = ClockChannelClient()
-
-    client.run(get_discord_token())
-
-
-if __name__ == '__main__':
+def main():
     arg_parser = ArgumentParser()
     arg_parser.add_argument('mode', choices=[MODE_CLOCK_CHANNEL])
+    arg_parser.add_argument('--discord-token', dest='discord_token', required=True)
+    arg_parser.add_argument('--clock-channel', dest='clock_channel_id', required=True)
 
     arguments = arg_parser.parse_args()
 
     if arguments.mode == MODE_CLOCK_CHANNEL:
         start_clock_channel(arguments)
+
+
+def start_clock_channel(arguments):
+    log.info("starting clock channel")
+    log.info(f"arguments={arguments}")
+    client = ClockChannelClient(
+        clock_channel_id=arguments.clock_channel_id,
+    )
+
+    client.run(arguments.discord_token)
+
+
+if __name__ == '__main__':
+    main()
